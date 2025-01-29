88e6c76d2fd1c65f2a3451fbd6f2c8fd501a1164
obah <olusojiobah@gmail.com>
2025-01-29 19:04:26 +0100

    changed poseidon to keccak256

--- fiat_shamir/src/fiat_shamir_transcript.rs
+++ fiat_shamir/src/fiat_shamir_transcript.rs
@@ -1,54 +1,27 @@
+use ark_ff::PrimeField;
+use sha3::{Digest, Keccak256};
 use std::marker::PhantomData;
 
-use ark_ff::PrimeField;
-use dusk_bls12_381::BlsScalar;
-use dusk_poseidon::{Domain, Hash};
-
-pub struct Transcript<'a, F> {
+pub struct Transcript<F: PrimeField> {
     _field: PhantomData<F>,
-    hasher: Hash<'a>,
+    hasher: Keccak256,
 }
 
-impl<'a, F: PrimeField> Transcript<'a, F> {
+impl<F: PrimeField> Transcript<F> {
     pub fn new() -> Self {
         Self {
             _field: PhantomData,
-            hasher: Hash::new(Domain::Other),
+            hasher: Keccak256::new(),
         }
     }
 
-    fn append(&mut self, preimage: &'a [BlsScalar]) {
-        self.hasher.update(&preimage)
+    pub fn append(&mut self, preimage: &[u8]) {
+        self.hasher.update(preimage)
     }
 
-    pub fn get_random_challenge(&mut self) -> F {
-        let final_hash = self.hasher.finalize();
+    pub fn get_random_challenge(&self) -> F {
+        let final_hash = self.hasher.clone().finalize(); //? this might be problematic
 
-        let hash_bytes = final_hash
-            .iter()
-            .flat_map(|scalar| scalar.to_bytes())
-            .collect::<Vec<u8>>();
-
-        F::from_le_bytes_mod_order(&hash_bytes)
+        F::from_le_bytes_mod_order(&final_hash)
     }
-}
-
-fn bytes_to_scalars(bytes: &[u8]) -> Result<Vec<BlsScalar>, &'static str> {
-    const SCALAR_SIZE: usize = 32;
-
-    if bytes.len() % SCALAR_SIZE != 0 {
-        return Err("Input byte slice length must be a multiple of Scalar size (32 bytes)");
-    }
-
-    let scalars = bytes
-        .chunks(SCALAR_SIZE)
-        .map(|chunk| {
-            let array: [u8; SCALAR_SIZE] = chunk
-                .try_into()
-                .expect("Chunk size is guaranteed to be SCALAR_SIZE");
-            BlsScalar::from_bytes(&array).expect("Invalid Scalar bytes")
-        })
-        .collect();
-
-    Ok(scalars)
 }
