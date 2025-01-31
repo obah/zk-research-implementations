510839186445a126c7a87ca9e152e7f2df536a44
obah <olusojiobah@gmail.com>
2025-02-01 00:53:19 +0100

    fix: resolved logic error in multilinear_polynomial_evaluation.rs

--- multilinear_polynomial/src/multilinear_polynomial_evaluation.rs
+++ multilinear_polynomial/src/multilinear_polynomial_evaluation.rs
@@ -1,90 +1,61 @@
 use ark_ff::PrimeField;
-use std::ops::{Add, AddAssign};
-
-type Hypercube<F> = Vec<(Vec<u8>, F)>;
-
-struct BHC<F> {
-    bits: Hypercube<F>,
-}
+use std::ops::Add;
 
 #[derive(Clone)]
 pub struct MultilinearPoly<F: PrimeField> {
     pub evaluation: Vec<F>,
-}
-
-impl<F: PrimeField> BHC<F> {
-    fn new(hypercube: Hypercube<F>) -> Self {
-        BHC { bits: hypercube }
-    }
-
-    fn generate_bhc(poly_evaluation: Vec<F>) -> Self {
-        let bits = poly_evaluation.len().ilog2() as usize;
-        let size = 1 << bits;
-
-        let hypercube: Hypercube<F> = (0..size)
-            .map(|i| {
-                let point = (0..bits).rev().map(|j| ((i >> j) & 1) as u8).collect();
-                (point, poly_evaluation[i])
-            })
-            .collect();
-
-        Self::new(hypercube)
-    }
-
-    fn pair_points(&self, bit: u8) -> Vec<(F, F)> {
-        let mut pairs = Vec::new();
-        let pair_index = 1 << bit;
-
-        for i in 0..pair_index {
-            if i + pair_index < self.bits.len() {
-                let (_, a) = &self.bits[i];
-                let (_, b) = &self.bits[i + pair_index];
-                pairs.push((a.clone(), b.clone()));
-            }
-        }
-
-        pairs
-    }
+    num_of_vars: usize,
 }
 
 impl<F: PrimeField> MultilinearPoly<F> {
     pub fn new(evaluations: Vec<F>) -> Self {
-        MultilinearPoly {
+        let num_of_vars: usize = evaluations.len().ilog2() as usize;
+
+        if evaluations.len() != 1 << num_of_vars {
+            panic!("Invalid evaluations");
+        }
+
+        Self {
             evaluation: evaluations,
+            num_of_vars,
         }
     }
 
-    fn iterpolate(points: (F, F), value: F) -> F {
-        let (y_0, y_1) = points;
+    fn pair_points(index: usize, num_of_vars: usize) -> Vec<(usize, usize)> {
+        let mut result = vec![];
+        let target_hc = num_of_vars - 1;
 
-        y_0 + (value * (y_1 - y_0))
+        for val in 0..(1 << target_hc) {
+            let inverted_index = num_of_vars - index - 1;
+            let insert_zero = insert_bit(val, inverted_index);
+            let insert_one = insert_zero | (1 << inverted_index);
+            result.push((insert_zero, insert_one));
+        }
+        result
     }
 
-    pub fn partial_evaluate(&self, value: F, bit: u8) -> Self {
-        let bhc = BHC::generate_bhc(self.evaluation.clone());
+    pub fn partial_evaluate(&self, bit: usize, value: &F) -> Self {
+        let mut result = vec![];
 
-        let paired_evaluations = bhc
-            .pair_points(bit)
-            .iter()
-            .map(|point| Self::iterpolate(*point, value))
-            .collect();
+        for (a, b) in MultilinearPoly::<F>::pair_points(bit, self.num_of_vars).into_iter() {
+            let a = self.evaluation[a];
+            let b = self.evaluation[b];
 
-        Self::new(paired_evaluations)
+            result.push(a + *value * (b - a));
+        }
+
+        Self::new(result)
     }
 
     pub fn evaluate(&self, values: Vec<F>) -> F {
+        if values.len() != self.num_of_vars {
+            panic!("Invalid number of values");
+        }
+
         let mut result = self.clone();
 
-        let mut bits = result.evaluation.len().ilog2() - 1;
-
         for value in values.iter() {
-            result = result.partial_evaluate(*value, bits.try_into().unwrap());
-
-            if bits == 0 {
-                break;
-            } else {
-                bits -= 1;
-            }
+            result = result.partial_evaluate(0, value);
         }
 
         result.evaluation[0]
@@ -109,18 +80,12 @@ impl<F: PrimeField> Add for MultilinearPoly<F> {
     }
 }
 
-impl<F: PrimeField> AddAssign for MultilinearPoly<F> {
-    fn add_assign(&mut self, other: Self) {
-        let mut result = vec![F::zero(); self.evaluation.len().max(other.evaluation.len())];
+fn insert_bit(value: usize, bit: usize) -> usize {
+    let high = value >> bit;
+    let mask = (1 << bit) - 1;
+    let low = value & mask;
 
-        for (i, &value) in self.evaluation.iter().enumerate() {
-            result[i] += value;
-        }
-
-        for (i, &value) in other.evaluation.iter().enumerate() {
-            result[i] += value;
-        }
-    }
+    high << bit + 1 | low
 }
 
 #[cfg(test)]
@@ -129,26 +94,14 @@ mod test {
     use ark_bn254::Fq;
 
     #[test]
-    fn it_pair_points_correctly() {
-        let evaluations = vec![Fq::from(0), Fq::from(1), Fq::from(2), Fq::from(3)];
-        let bhc = BHC::generate_bhc(evaluations);
-
-        let pairs = bhc.pair_points(1);
-        assert_eq!(
-            pairs,
-            vec![(Fq::from(0), Fq::from(2)), (Fq::from(1), Fq::from(3))]
-        );
-    }
-
-    #[test]
     fn it_partially_evaluates_any_multilinear() {
         let evaluations = vec![Fq::from(0), Fq::from(0), Fq::from(3), Fq::from(10)];
         let polynomial = MultilinearPoly::new(evaluations);
 
         let value_a = Fq::from(5);
-        let bit_a = 1;
+        let bit_a = 0;
 
-        let result = polynomial.partial_evaluate(value_a, bit_a);
+        let result = polynomial.partial_evaluate(bit_a, &value_a);
 
         assert_eq!(result.evaluation, vec![Fq::from(15), Fq::from(50)]);
     }
