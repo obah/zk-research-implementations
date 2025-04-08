624083a63b155a37aecbe07f954b284195a972d5
obah <olusojiobah@gmail.com>
2025-04-08 01:33:40 +0100

    chore: added tests

--- fft/src/fft.rs
+++ fft/src/fft.rs
@@ -78,3 +78,52 @@ fn split_poly<F: PrimeField>(poly: &[F]) -> (Vec<F>, Vec<F>) {
 
     ((poly_even), (poly_odd))
 }
+
+#[cfg(test)]
+
+mod test {
+    use ark_bn254::Fq;
+    use univariate_polynomial::univariate_polynomial_dense::UnivariatePoly;
+
+    use super::{fft_evaluate, fft_interpolate, split_poly};
+
+    #[test]
+    fn it_splits_poly_correctly() {
+        //test with P = x3 + 2x2 -14x + 2
+        let poly = &[Fq::from(2), Fq::from(-14), Fq::from(2), Fq::from(1)];
+
+        let (p_even, p_odd) = split_poly(&poly);
+
+        let expected_p_even = vec![Fq::from(2), Fq::from(2)];
+
+        let expected_p_odd = vec![Fq::from(-14), Fq::from(1)];
+
+        assert_eq!(p_even, expected_p_even);
+        assert_eq!(p_odd, expected_p_odd);
+    }
+
+    #[test]
+    fn it_evaluates_poly() {
+        //test with P = 4x3 + 3x2 + 2x + 1
+        let points = vec![Fq::from(1), Fq::from(2), Fq::from(3), Fq::from(4)];
+        let poly = UnivariatePoly::new(points);
+
+        let evaluations = fft_evaluate(&poly);
+
+        let expected_evaluations = vec![Fq::from(), Fq::from(), Fq::from(), Fq::from()];
+
+        assert_eq!(evaluations, expected_evaluations);
+    }
+
+    #[test]
+    fn it_interpolates_poly() {
+        //same poly as evaluate test
+        let evaluations = vec![Fq::from(), Fq::from(), Fq::from(), Fq::from()];
+
+        let poly = fft_interpolate(&evaluations);
+
+        let expected_points = vec![Fq::from(1), Fq::from(2), Fq::from(3), Fq::from(4)];
+
+        assert_eq!(poly.coefficient, expected_points);
+    }
+}
